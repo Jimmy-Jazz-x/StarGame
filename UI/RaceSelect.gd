6fc28f66e14c06d377d3f9edb3804ee804ef726c
Jimmy-Jazz-x <mathew.p.marshall@gmail.com>
2024-01-14 19:44:59 -0400

    Main Menu

---
+++ UI/RaceSelect.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_borg_pressed():
+	print("Borg")
+	pass # Replace with function body.
+
+
+func _on_nanites_pressed():
+	print("Nanite")
+	pass # Replace with function body.
+
+
+func _on_flood_pressed():
+	print("Flood")
+	pass # Replace with function body.
