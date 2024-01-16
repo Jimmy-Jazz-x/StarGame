a426e8300494738eb68fb494414d6c079c7644c6
Jimmy-Jazz-x <mathew.p.marshall@gmail.com>
2024-01-15 23:13:32 -0400

    Cam Movement Fixed

--- Gameplay/Camera2D.gd
+++ Gameplay/Camera2D.gd
@@ -3,6 +3,7 @@ extends Camera2D
 var mouse_start_pos
 var screen_start_position
 var ZoomSpeed = Vector2(0.1,0.1)
+var MoveSpeed = 20
 var dragging = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,9 +22,21 @@ func _process(delta):
 		dragging = false
 	if InputEventMouseMotion and dragging:
 		print(str(get_global_mouse_position()))
-		self.position= zoom * (mouse_start_pos - get_global_mouse_position()) + screen_start_position
+		self.position = (mouse_start_pos - get_global_mouse_position()) + screen_start_position
 		
 	elif Input.is_action_just_released("Zoom In"):
 		zoom += ZoomSpeed
+		self.position = get_global_mouse_position()
 	elif Input.is_action_just_released("Zoom Out"):
 		zoom -= ZoomSpeed
+		self.position = get_global_mouse_position()
+		
+		
+	if Input.is_action_pressed("CamUp"):
+		self.position += Vector2(0,-20)
+	if Input.is_action_pressed("CamDown"):
+		self.position += Vector2(0,20)
+	if Input.is_action_pressed("CamLeft"):
+		self.position += Vector2(-20,-0)
+	if Input.is_action_pressed("CamRight"):
+		self.position += Vector2(20,0)
